38ddd2d45eba6e3e27cd52ccdaa3d9395e862128
thebluetropics <thebluetropics@gmail.com>
2024-10-30 19:03:12 +0700

    add into creative inventory tab

--- fabric/src/main/java/io/github/thebluetropics/solidgrassblock/SolidGrassBlockMod.java
+++ fabric/src/main/java/io/github/thebluetropics/solidgrassblock/SolidGrassBlockMod.java
@@ -3,8 +3,13 @@ package io.github.thebluetropics.solidgrassblock;
 import io.github.thebluetropics.solidgrassblock.block.ModBlocks;
 import io.github.thebluetropics.solidgrassblock.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
 
 public class SolidGrassBlockMod implements ModInitializer {
   public static final String ID = "solidgrassblock";
@@ -14,5 +19,11 @@ public class SolidGrassBlockMod implements ModInitializer {
   public void onInitialize() {
     ModBlocks.initialize();
     ModItems.initialize();
+
+    ItemGroupEvents.MODIFY_ENTRIES_ALL.register((itemGroup, fabricItemGroupEntries) -> {
+      if (Objects.equals(itemGroup, ItemGroups.getDefaultTab())) {
+        fabricItemGroupEntries.add(ModItems.SOLID_GRASS_BLOCK.getDefaultStack());
+      }
+    });
   }
 }
