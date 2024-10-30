8b47875348d841ea771164739b7bc668f1c4c267
thebluetropics <thebluetropics@gmail.com>
2024-10-30 19:11:54 +0700

    remove unnecessary `SpreadableBlockMixin.java`

--- fabric/src/main/java/io/github/thebluetropics/solidgrassblock/block/SolidGrassBlock.java
+++ fabric/src/main/java/io/github/thebluetropics/solidgrassblock/block/SolidGrassBlock.java
@@ -1,9 +1,16 @@
 package io.github.thebluetropics.solidgrassblock.block;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.GrassBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 
 public class SolidGrassBlock extends GrassBlock {
   public SolidGrassBlock(Settings settings) {
     super(settings);
   }
+
+  @Override
+  protected void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) { /* ... */ }
 }
