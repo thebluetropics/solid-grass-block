9e8b6cf7960723be7c6ec91b82d6ed20742354c8
thebluetropics <thebluetropics@gmail.com>
2024-12-02 13:33:19 +0700

    fix `build.gradle.kts`
    fixes a fatal maven artifact naming and versioning, with some quality of life addition to the `build.gradle.kts`

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -3,14 +3,12 @@ plugins {
   id("maven-publish")
 }
 
-val minecraftVersion = properties["minecraft_version"] as String
-
 base {
   archivesName = "${properties["artifact_name"].toString()}-fabric"
 }
 
 dependencies {
-  minecraft("com.mojang:minecraft:${minecraftVersion}")
+  minecraft("com.mojang:minecraft:${properties["minecraft_version"].toString()}")
   mappings("net.fabricmc:yarn:${properties["mappings_version"].toString()}:v2")
   modImplementation("net.fabricmc:fabric-loader:${properties["fabric_loader_version"].toString()}")
   modImplementation("net.fabricmc.fabric-api:fabric-api:${properties["fabric_api_version"].toString()}")
@@ -28,13 +26,43 @@ loom {
 
 java {
   toolchain.languageVersion = JavaLanguageVersion.of(21)
+
+  sourceCompatibility = JavaVersion.VERSION_21
+  targetCompatibility = JavaVersion.VERSION_21
+
   withSourcesJar()
+}
+
+tasks.withType(JavaCompile::class.java).configureEach {
+  options.encoding = "UTF-8"
+  options.release = 21
+}
+
+tasks.processResources {
+  filesMatching("fabric.mod.json") {
+    filter { line ->
+      Regex("%([a-z_]+)%").replace(line) { match ->
+        properties[match.groupValues[1]]?.toString() ?: match.value
+      }
+    }
+  }
+}
+
+tasks.named<Jar>("jar") {
+  from("LICENSE") {
+    rename {
+      "LICENSE-${properties["artifact_name"]}"
+    }
+  }
 }
 
 publishing {
   publications {
     create<MavenPublication>("mavenJava") {
-      artifactId = base.archivesName.get() + "-${version}"
+      groupId    = project.group.toString()
+      artifactId = project.base.archivesName.get()
+      version    = project.version.toString()
+
       from(components["java"])
     }
   }
@@ -42,7 +70,7 @@ publishing {
   repositories {
     maven {
       name = "Mods"
-      url = uri(layout.buildDirectory.dir("repository"))
+      url = uri("file://${projectDir}/repository")
     }
     maven {
       name = "GithubPackages"
