739d69072f5d87825eabd4cf8ddc50680824a60d
thebluetropics <thebluetropics@gmail.com>
2024-12-15 14:07:01 +0700

    modify `getPlacementState` for grass block

--- src/main/java/io/github/thebluetropics/solidgrassblock/mixin/SnowyBlockMixin.java
+++ src/main/java/io/github/thebluetropics/solidgrassblock/mixin/SnowyBlockMixin.java
@@ -5,9 +5,12 @@ import io.github.thebluetropics.solidgrassblock.block.SolidMyceliumBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.SnowyBlock;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.Items;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,6 +33,33 @@ public class SnowyBlockMixin {
     }
 
     if (state.isOf(Blocks.MYCELIUM) && direction.equals(Direction.UP) && !neighborState.isIn(BlockTags.SNOW)) {
+      if (!SolidMyceliumBlock.canMyceliumSurvive(state, world, pos)) {
+        info.setReturnValue(Blocks.DIRT.getDefaultState());
+      }
+    }
+  }
+
+  /// Force grass block to immediately turn into dirt when there's a solid block on top of it upon placing.
+  @Inject(
+    at = @At("HEAD"),
+    method = "getPlacementState",
+    cancellable = true
+  )
+  private void getPlacementState(ItemPlacementContext context, CallbackInfoReturnable<BlockState> info) {
+    var world = context.getWorld();
+
+    var stack = context.getStack();
+
+    var pos = context.getBlockPos();
+    var state = world.getBlockState(pos);
+
+    if (stack.isOf(Items.GRASS_BLOCK)) {
+      if (!SolidGrassBlock.canGrassBlockSurvive(state, world, pos)) {
+        info.setReturnValue(Blocks.DIRT.getDefaultState());
+      }
+    }
+
+    if (stack.isOf(Items.MYCELIUM)) {
       if (!SolidMyceliumBlock.canMyceliumSurvive(state, world, pos)) {
         info.setReturnValue(Blocks.DIRT.getDefaultState());
       }
