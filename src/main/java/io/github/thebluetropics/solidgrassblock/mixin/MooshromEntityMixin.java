6709a114b2efbff4e7f57d1f511fb93ff1ebb02c
thebluetropics <thebluetropics@gmail.com>
2024-12-17 17:16:43 +0700

    add `MooshroomEntity` mixin

---
+++ src/main/java/io/github/thebluetropics/solidgrassblock/mixin/MooshromEntityMixin.java
@@ -0,0 +1,26 @@
+package io.github.thebluetropics.solidgrassblock.mixin;
+
+import io.github.thebluetropics.solidgrassblock.block.ModBlocks;
+import net.minecraft.entity.passive.MooshroomEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MooshroomEntity.class)
+public class MooshromEntityMixin {
+  @Inject(
+    at = @At("HEAD"),
+    method = "getPathfindingFavor",
+    cancellable = true
+  )
+  private void getPathfindingFavor(BlockPos pos, WorldView world, CallbackInfoReturnable<Float> info) {
+    var lowerBlockState = world.getBlockState(pos.down());
+
+    if (lowerBlockState.isOf(ModBlocks.SOLID_MYCELIUM)) {
+      info.setReturnValue(10.0f);
+    }
+  }
+}
