6199dec3ef33004c6ef026be5c482a369203f2d7
thebluetropics <thebluetropics@gmail.com>
2025-05-23 16:15:51 +0700

    refactor AbstractHorseEntityMixin class

--- src/main/java/io/github/thebluetropics/solidgrassblock/mixin/AbstractHorseEntityMixin.java
+++ src/main/java/io/github/thebluetropics/solidgrassblock/mixin/AbstractHorseEntityMixin.java
@@ -8,8 +8,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Objects;
-
 @Mixin(AbstractHorseEntity.class)
 public class AbstractHorseEntityMixin {
 	@Shadow
@@ -27,13 +25,12 @@ public class AbstractHorseEntityMixin {
 	)
 	private void tickMovement(CallbackInfo info) {
 		var entity = (AbstractHorseEntity) (Object) this;
+		var world = entity.getWorld();
 
-		if (!entity.getWorld().isClient && entity.isAlive()) {
-			if (entity.eatsGrass()) {
-				var lowerBlockState = entity.getWorld().getBlockState(entity.getBlockPos().down());
-
-				if (!entity.isEatingGrass()) {
-					if (lowerBlockState.isOf(ModBlocks.SOLID_GRASS_BLOCK) && !entity.hasPassengers() && Objects.equals(entity.getRandom().nextInt(300), 0)) {
+		if (!world.isClient) {
+			if (entity.isAlive() && entity.eatsGrass() && !entity.isEatingGrass()) {
+				if (world.getBlockState(entity.getBlockPos().down()).isOf(ModBlocks.SOLID_GRASS_BLOCK) && !entity.hasPassengers()) {
+					if (!(entity.getRandom().nextInt(300) != 0)) {
 						entity.setEatingGrass(true);
 					}
 				}
