f8d962cf683cd563fa7882a33793f3ccd3fc14b1
thebluetropics <thebluetropics@gmail.com>
2025-05-23 16:19:48 +0700

    fix comment

--- src/main/java/io/github/thebluetropics/solidgrassblock/block/SolidNyliumBlock.java
+++ src/main/java/io/github/thebluetropics/solidgrassblock/block/SolidNyliumBlock.java
@@ -91,7 +91,7 @@ public class SolidNyliumBlock extends Block implements Fertilizable {
 		return realisticOpacity < world.getMaxLightLevel();
 	}
 
-	/**  Checks whether a solid nylium block can survive */
+	/** Checks whether a solid nylium block can survive */
 	private static boolean canSolidNyliumSurvive(BlockState state, WorldView world, BlockPos pos) {
 		for (Direction direction : Direction.values()) {
 			var checkPos = pos.offset(direction);
